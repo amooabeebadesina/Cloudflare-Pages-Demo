6bb022f38de8bae148f0f71fe3a81615e1613607
Abeeb <amooabeeb0@gmail.com>
2021-11-09 00:18:04 +0000

    Add local store

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,49 @@
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import { FaThumbsDown, FaThumbsUp } from "react-icons/all";
+import {useState} from "react";
 
 const Post = (props) => {
+
+    const { id, likeStore, dislikeStore, onLikeUpdate, onDislikeUpdate } = props;
+
+    const [ likes, setLikes ] = useState(props.likes);
+    const [ dislikes, setDislikes ] = useState(props.dislikes);
+
+    const handlePostUpdate = (data) => {
+        return fetch(`${process.env.REACT_APP_API_BASE_URL}/posts/${id}`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                return true;
+            })
+            .catch((error) => {
+                return false;
+            });
+    }
+
+    const canLike = () => !likeStore.hasOwnProperty(id);
+
+    const canDislike = () => !dislikeStore.hasOwnProperty(id);
+
+    const onClickLike = async () => {
+        const updateLikes = await handlePostUpdate({ likes: likes + 1});
+        if (updateLikes) {
+            setLikes((prev) => prev + 1);
+            onLikeUpdate(id);
+        }
+    }
+
+    const onClickDislike = async () => {
+        const updatedisLikes = await handlePostUpdate({ dislikes: dislikes + 1});
+        if (updatedisLikes) {
+            setDislikes((prev) => prev + 1);
+            onDislikeUpdate(id);
+        }
+    }
+
     return (
         <Card className='post-card'>
             <Card.Header>
@@ -14,8 +55,12 @@ const Post = (props) => {
             <Card.Footer className='post-card__footer'>
                 <Card.Text className='post-card__author'>{props.username}</Card.Text>
                 <Card.Text className='post-card__stats'>
-                    <span className='post-card__stat'><FaThumbsUp color='#008800'/>{props.likes}</span>
-                    <span className='post-card__stat'><FaThumbsDown color='#AB0101' />{props.dislikes}</span>
+                    <button className='post-card__stat' disabled={!canLike()} onClick={onClickLike}>
+                        <FaThumbsUp color='#008800'/>{likes}
+                    </button>
+                    <button className='post-card__stat' disabled={!canDislike()} onClick={onClickDislike}>
+                        <FaThumbsDown color='#AB0101'/>{dislikes}
+                    </button>
                 </Card.Text>
             </Card.Footer>
         </Card>
@@ -23,16 +68,23 @@ const Post = (props) => {
 }
 
 Post.propTypes = {
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     likes: PropTypes.number,
-    dislikes: PropTypes.number
+    dislikes: PropTypes.number,
+    likeStore: PropTypes.object,
+    dislikeStore: PropTypes.object,
+    onLikeUpdate: PropTypes.func,
+    onDislikeUpdate: PropTypes.func
 }
 
 Post.defaultProps = {
     likes: 0,
-    dislikes: 0
+    dislikes: 0,
+    likeStore: {},
+    dislikeStore: {}
 }
 
 export default Post;
--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -6,10 +6,19 @@ const PostContainer = (props) => {
     const { refetchPosts, onShowToast } = props;
 
     const [posts, setPosts] = useState([]);
+    const [likeStore, setLikeStore] = useState({});
+    const [dislikeStore, setDislikeStore] = useState({});
 
     const handleError = useCallback(() => {
         onShowToast({ type: 'danger', message: 'Error fetching posts'});
     }, [])
+
+    useEffect(() => {
+        const likes = localStorage.getItem('likes') ? JSON.parse(localStorage.getItem('likes')) : {};
+        const dislikes = localStorage.getItem('dislikes') ? JSON.parse(localStorage.getItem('dislikes')) : {};
+        setLikeStore(likes);
+        setDislikeStore(dislikes);
+    }, []);
 
     useEffect(() => {
         if (refetchPosts) {
@@ -23,12 +32,31 @@ const PostContainer = (props) => {
         }
     }, [refetchPosts, handleError]);
 
+    const handleLikeUpdate = (id) => {
+        const storage = {...likeStore, [id]: true};
+        setLikeStore((prev) => ({...prev, [id]: true}));
+        localStorage.setItem('likes', JSON.stringify(storage));
+    }
+
+    const handleDislikeUpdate = (id) => {
+        const storage = {...dislikeStore, [id]: true};
+        setDislikeStore((prev) => ({...prev, [id]: true}));
+        localStorage.setItem('dislikes', JSON.stringify(storage));
+    }
+
     return (
         <>
             <div className='posts-content'>
                 {
                     posts.map((post, key) => (
-                        <Post {...post} key={key}/>
+                        <Post
+                            {...post}
+                            key={key}
+                            likeStore={likeStore}
+                            dislikeStore={dislikeStore}
+                            onLikeUpdate={handleLikeUpdate}
+                            onDislikeUpdate={handleDislikeUpdate}
+                        />
                     ))
                 }
             </div>
